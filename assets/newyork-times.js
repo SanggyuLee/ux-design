4f09114120b9b6e5837446f8bc46f51aa906e02f
이상규 <lp31004@gmail.com>
2016-10-22 01:04:17 +0900

    CSS is modified
    * Background-attachment is added
    * Some minor issues are fixed

--- assets/newyork-times.js
+++ assets/newyork-times.js
@@ -6,24 +6,53 @@ ipc.on('get-NYTimes-articles', function(event, args) {
 
 	function slow_motion() {
 		opacity += 0.04
-		document.querySelector(".articles").style.opacity = opacity
+		document.querySelector(".article-box").style.opacity = opacity
 	
 		if(opacity >= 1)
 			clearInterval(timer)
 	}
 
-	console.log(args.response.docs)
-
 	var articles = args.response.docs
 	var output = '';
 	for(var i = 0; i < articles.length; i++) {
-		output += `<div>${articles[i].snippet}</div>`
+		var img_src = ""
+		var display = "none"
+		var multimedia = articles[i].multimedia
+
+		for(var j = 0; j < multimedia.length; j++) {
+			if(multimedia[j].subtype === "thumbnail") {
+				img_src = "http://www.nytimes.com/" + multimedia[j].url
+				display = "inline"
+				break;
+			}
+		}
+
+		output += `
+			<div class="article">
+				<div class="thumbnail" style="display: ${display}">
+					<img src=${img_src} />
+				</div>
+
+				<div class="content">
+					<div class="headline">
+						<a href="${articles[i].web_url}"> ${articles[i].headline.main} </a>
+					</div>
+	
+					<div class="snippet">
+						${articles[i].snippet}
+					</div>
+				</div>
+			</div>
+			`
 		output += `<br>`
+
+		console.log(articles[i].multimedia)
 	}
+
 
 	timer = setInterval(slow_motion, 50)
 
-	document.querySelector(".articles").style.opacity = opacity
-	document.querySelector(".articles").style.display = "flex"
-	document.querySelector(".articles").innerHTML = output
+	document.querySelector(".article-box").style.opacity = opacity
+	document.querySelector(".article-box").style.display = "flex"
+	document.querySelector(".article-box").innerHTML = output
 })
--- assets/twitter.js
+++ assets/twitter.js
@@ -16,7 +16,7 @@ ipc.on('get-twitter-trend', function(event, path) {
 
 	function slow_motion() {
 		opacity += 0.04
-		document.querySelector(".twitter-result").style.opacity = opacity
+		document.querySelector(".twitter-result-box").style.opacity = opacity
 	
 		if(opacity >= 1)
 			clearInterval(timer)
@@ -24,7 +24,7 @@ ipc.on('get-twitter-trend', function(event, path) {
 
 	timer = setInterval(slow_motion, 50)
 
-	document.querySelector(".twitter-result").style.opacity = opacity
-	document.querySelector(".twitter-result").style.display = "flex"
-	document.querySelector(".twitter-result").innerHTML = output
+	document.querySelector(".twitter-result-box").style.opacity = opacity
+	document.querySelector(".twitter-result-box").style.display = "flex"
+	document.querySelector(".twitter-result-box").innerHTML = output
 })
