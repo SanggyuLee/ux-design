528b9753d732a119389435c20447948e5a408ae1
이상규 <lp31004@gmail.com>
2016-12-03 00:22:49 +0900

    hm

--- assets/twitter.js
+++ assets/twitter.js
@@ -7,14 +7,30 @@ console.log(wordcloud.isSupported);
 ipc.send('request-twitter-country', '');
 
 ipc.on('get-twitter-country', function(event, tweets) { 
-	console.log(tweets);
 	let onRegionClick = (element, code, region) => {
 		let isExist = false;
 		for(let i = 0; i < tweets.length; i++) {
 			if(tweets[i].name === region) {
 				isExist = true;
+				document.body.style.display = "relative";
 				document.getElementById("vmap").style.display = "none";
-				document.getElementById("twitter").style.display = "block";
+				let bar = document.getElementById("bar");
+				bar.style.display = "flex";
+
+				let worldmap = document.createElement("img");
+				worldmap.src = "./assets/img/worldmap.png";
+				worldmap.style.cursor = "pointer";
+				worldmap.id = "worldmap";
+				worldmap.onclick = (event) => {
+					location.reload();
+				};
+
+				let label = document.getElementById("label");
+				label.innerHTML = region;
+
+				bar.appendChild(worldmap);
+
+				document.getElementById("twitter").style.display = "flex";
 				ipc.send('request-twitter-trend', tweets[i].woeid);
 
 				break;
@@ -88,27 +104,33 @@ ipc.on('get-twitter-trend', (event, path) => {
 	for(let i = 0; i < tweets.length; i++) {
 		let temp = [];
 		temp.push(tweets[i].name);
-		temp.push(tweets.length - i / 4);
+		temp.push(tweets.length - i / 2);
 
 		list.push(temp);
 	}
 
+	let hover = (a, b, c) => {
+		console.log(a);
+		console.log(b);
+		console.log(c);
+	};
+
 	let selectTweet = (item) => {
-		console.log("selectTweet");
+		console.log(item);
+		document.getElementById("label").innerHTML = item[0];
 		document.getElementById("twitter").style.display = "none";
 		ipc.send('request-NYTimes-articles', item[0]);
 	};
 
 	wordcloud(document.getElementById("twitter"), 
 			{
-				gridSize: 10, 
 				weightFactor: 4, 
 				classes: 'tweet',
 				color: 'sanggyu',
 				fontFamily: 'Chela One',
 				backgroundColor: 'transparent',
-				minSize: 20,
 				list: list,
+				hover: hover,
 				click: selectTweet
 			}
 	);
